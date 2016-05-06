57b7859af055eee4d5ec1429f6a175953a4ba817
samk17cmutpm <samk17cmutpm@gmail.com>
2016-05-06 19:17:38 +0700

    indicator

--- app/src/main/java/novahub/vn/npr4dogs/piles/PilesStepFiveFragment.java
+++ app/src/main/java/novahub/vn/npr4dogs/piles/PilesStepFiveFragment.java
@@ -3,9 +3,11 @@ package novahub.vn.npr4dogs.piles;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import novahub.vn.npr4dogs.BaseFragment;
 import novahub.vn.npr4dogs.R;
@@ -31,6 +33,21 @@ public class PilesStepFiveFragment extends BaseFragment implements PilesContract
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         root = inflater.inflate(R.layout.fragment_piles_step_five, container, false);
+        TextView textViewAdd1 = (TextView) root.findViewById(R.id.textViewAdd1);
+        TextView textViewAdd2 = (TextView) root.findViewById(R.id.textViewAdd2);
+        TextView textViewAdd3 = (TextView) root.findViewById(R.id.textViewAdd3);
+        TextView textViewAdd4 = (TextView) root.findViewById(R.id.textViewAdd4);
+
+        String htmlString1 = "<u>npr4dogs Genetics Lab</u>";
+        String htmlString2 = "<u>UC Davis</u>";
+        String htmlString3 = "<u>Old Davis Road</u>";
+        String htmlString4 = "<u>Davis, CA 95616</u>";
+
+        textViewAdd1.setText(Html.fromHtml(htmlString1));
+        textViewAdd2.setText(Html.fromHtml(htmlString2));
+        textViewAdd3.setText(Html.fromHtml(htmlString3));
+        textViewAdd4.setText(Html.fromHtml(htmlString4));
+        
         return root;
     }
 
